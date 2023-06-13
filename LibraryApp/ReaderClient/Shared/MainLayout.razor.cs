c0d9334fc481c26ae237d0c3332006acdd92a2f1
Ádám Karátsony <akaratsony@gmail.com>
2023-06-13 10:21:55 +0200

    Modofied main layout.

---
+++ LibraryApp/ReaderClient/Shared/MainLayout.razor.cs
@@ -0,0 +1,62 @@
+﻿using EventAggregator.Blazor;
+using ReaderClient.Data;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ReaderClient.Shared
+{
+    public partial class MainLayout : IHandle<LoadEvent>
+    {
+        private bool isDrawerOpen = true;
+        private bool loading = false;
+
+        private MudTheme myCustomTheme = new MudTheme()
+        {
+            Palette = new PaletteLight()
+            {
+                Primary = "#FFFFFF",
+                Secondary = Colors.Green.Accent4,
+                AppbarBackground = Colors.Green.Default,
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = "#00e676ff",
+                Secondary = "#FFFFFF",
+                Tertiary = "#00C853",
+                Background = "#37474fff",
+                Dark = "#263238ff",
+                DrawerBackground = "#263238ff",
+                AppbarBackground = "#263238ff",
+                DrawerText = "#FFFFFF",
+                DrawerIcon = "#FFFFFF",
+                Surface = "#263238ff",
+                TableStriped = "#37474fff",
+                TableLines = "#424242ff",
+            },
+        };
+
+        [Inject]
+        public IEventAggregator? EventHandler { get; set; }
+
+        public async Task HandleAsync(LoadEvent msg)
+        {
+            loading = msg.State;
+            await InvokeAsync(StateHasChanged);
+        }
+
+        protected override void OnInitialized()
+        {
+            EventHandler!.Subscribe(this);
+        }
+
+        private void ToggleDrawer()
+        {
+            isDrawerOpen = !isDrawerOpen;
+        }
+
+        private string GetAvatarSize()
+        {
+            return isDrawerOpen ? "width:56px;height:56px;" : "width:40px;height:40px;";
+        }
+    }
+}
